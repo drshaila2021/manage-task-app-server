7ad7938b30e965569f0f74e2db8ada24ecd7f1e2
Shaila Nasrin <drshaila2021@gmail.com>
2022-07-01 00:12:32 -0500

    error fix

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
 const app = express();
-const port = 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
